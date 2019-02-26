83d9eb507f705ab518c6d6d9b1c86ba4e0254eb2
Eric Wu <zewu999@gmail.com>
2019-02-26 10:31:38 -0500

    final

---
+++ Assets/__Scripts/Circle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Circle : MonoBehaviour {
+
+	public GameObject prefab;
+
+	private GameObject _myNewObject;
+
+    int num = 12;
+
+    void Start() {
+
+		Vector3 mid = transform.position;
+		for (int i = 0; i < (num/3); i++) {
+			int a = i * 90;
+			Vector3 pos = RandomCircle(mid, 3.0f ,a);
+			_myNewObject = Instantiate(prefab, pos, Quaternion.identity);
+			_myNewObject.GetComponent<Renderer> ().material.color = Color.red;
+			_myNewObject.tag = "Red";
+		}
+		for (int i = 0; i < (num/3); i++) {
+			int a = i * 90 + 30;
+			Vector3 pos = RandomCircle(mid, 3.0f ,a);
+			_myNewObject = Instantiate(prefab, pos, Quaternion.identity);
+			_myNewObject.GetComponent<Renderer> ().material.color = Color.gray;
+			_myNewObject.tag = "Gray";
+		}
+		for (int i = 0; i < (num/3); i++) {
+			int a = i * 90 + 60;
+			Vector3 pos = RandomCircle(mid, 3.0f ,a);
+			_myNewObject = Instantiate(prefab, pos, Quaternion.identity);
+			_myNewObject.GetComponent<Renderer> ().material.color = Color.green;
+			_myNewObject.tag = "Green";
+		}
+	}
+	Vector3 RandomCircle(Vector3 center, float radius,int a) {
+		Debug.Log(a);
+		float ang = a;
+		Vector3 pos;
+		pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+		pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+		pos.y = center.y;
+		return pos;
+	}
+}
---
+++ Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public Text countText;
+	public Text winText;
+
+    private Rigidbody rb;
+    private int score;
+
+    // Use this for initialization
+    void Start () {
+		rb = GetComponent<Rigidbody>();
+		score = 0;
+		SetScoreText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * 8);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Red")){
+			other.gameObject.SetActive (false);
+			score = score + 1;
+			SetScoreText ();
+		}
+		if(other.gameObject.CompareTag("Gray")){
+			other.gameObject.SetActive (false);
+			score = score + 2;
+			SetScoreText ();
+		}
+		if(other.gameObject.CompareTag("Green")){
+			other.gameObject.SetActive (false);
+			score = score + 3;
+			SetScoreText ();
+		}
+	}
+
+	void SetScoreText() {
+		if (score == 24) {
+			winText.text = "You win!";
+		}
+		countText.text = "Score: " + score.ToString ();
+	}
+}
